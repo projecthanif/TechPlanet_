216c70654f162f4615d42ddbf9ab3857069fabf3
projecthanif <iamustapha213@gmail.com>
2024-02-02 12:38:12 +0100

    want to make a status details

--- app/Http/Controllers/PaymentGateway/PaymentGateWay.php
+++ app/Http/Controllers/PaymentGateway/PaymentGateWay.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\PaymentGateway;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Status extends Controller
+class PaymentGateWay extends Controller
 {
     //
 }
